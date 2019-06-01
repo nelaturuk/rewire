332da04e1745805b94fe1a67830cf70b92a17a2c
Keerthi Nelaturu <keerthi.nelaturu@scotiabank.com>
2019-06-01 16:35:54 -0400

    Adding signin content in home view

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { web3Connect } from '../../../store/web3Reducer'
-import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 export class HomeView extends Component {
   constructor(props) {
@@ -18,9 +20,47 @@ export class HomeView extends Component {
   render() {
     return (
       <div>
-        <h4>Welcome!</h4>
-        <div>Connected to Web3 :  {JSON.stringify(this.props.web3Wrap.isConnected)}</div>
-        <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+        <div style={{float:'left', width: 350}}><img
+          width="350px"
+          alt='This is a duck, because Redux!'
+          src={'https://source.unsplash.com/random'} /></div>
+        <div><form noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+
+        </form></div>
       </div>
     );
   }
@@ -31,7 +71,7 @@ export class HomeView extends Component {
 // }
 
 const mapStateToProps = (state) => ({
-  web3Wrap: state.web3Wrap 
+  web3Wrap: state.web3Wrap
 })
 
 const mapDispatchToProps = {
--- src/routes/Invest/components/Invest.js
+++ src/routes/Invest/components/Invest.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { increment, doubleAsync } from '../modules/invest'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 export class Invest extends Component {
   constructor(props) {
@@ -12,22 +14,25 @@ export class Invest extends Component {
     return (
       <div style={{ margin: '0 auto' }} >
         hello invest
+        <Button variant="contained" color="primary">
+          Hello World
+    </Button>
       </div>
     )
   }
 }
 
 Invest.propTypes = {
-  
+
 }
 
 const mapDispatchToProps = {
-  increment : () => increment(1),
+  increment: () => increment(1),
   doubleAsync
 }
 
 const mapStateToProps = (state) => ({
-  web3Wrap : state.web3Wrap
+  web3Wrap: state.web3Wrap
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Invest)
